94ff2dc55ddfa6928975e5fcf26d86f2595d7b72
R-R-Freitas <rodrigoaz8@gmail.com>
2022-10-10 23:05:30 -0300

    find the duplicate - iterative

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,10 +1,19 @@
 def find_duplicate(nums):
+    # try:
+    #     if len(nums) < 2 or min(nums) < 0:
+    #         return False
+    #     if nums[0] in nums[1:]:
+    #         return nums[0]
+    #     else:
+    #         return find_duplicate(nums[1:])
+    # except TypeError:
+    #     return False
     try:
         if len(nums) < 2 or min(nums) < 0:
             return False
-        if nums[0] in nums[1:]:
-            return nums[0]
-        else:
-            return find_duplicate(nums[1:])
+        for i in range(len(nums)):
+            if nums[i] in nums[(i+1):]:
+                return nums[i]
+        return False
     except TypeError:
         return False
