37ed55ebdcbc787d252173c1e3da5e8091fa417c
R-R-Freitas <rodrigoaz8@gmail.com>
2022-10-10 22:28:33 -0300

    feat: palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    if word is None or len(word) == 0:
+        return False
+    is_palindrome = True
+    for i in range(0, len(word) // 2):
+        if word[i] != word[-(i + 1)]:
+            is_palindrome = False
+    return is_palindrome
